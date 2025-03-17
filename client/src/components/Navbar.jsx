8f5c4f49efbc8f14e1f491062bb21349b5b03d14
glsaacke <glsaacke@crimson.ua.edu>
2025-03-17 11:55:19 -0500

    Home initialization

---
+++ client/src/components/Navbar.jsx
@@ -0,0 +1,12 @@
+import './styles/Navbar.css'
+
+const Navbar = () => {
+    return ( 
+        <div className="navbar-container">
+            <img src="./assets/icons/G-Icon.svg"/>
+
+        </div>
+     );
+}
+ 
+export default Navbar;
