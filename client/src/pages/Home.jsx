2ef98519b00d1ed4d301ef3a987a787837cbe9d2
glsaacke <143575030+glsaacke@users.noreply.github.com>
2025-03-25 16:55:40 -0500

    home title

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -4,7 +4,12 @@ const Home = () => {
     return ( 
         <div className="home-container">
             {/* <img src="./src/assets/PeaksPng.PNG" alt="Blue Peaks background"/> */}
-            <div className="home-content"></div>
+            <div className="home-content">
+                <div className="home-title">
+                    <h1>Gavin Saacke</h1>
+                    <h4>Full Stack Software Engineer</h4>
+                </div>
+            </div>
             <div className="home-bg"></div>
         </div>
      );
